c5d03156e96ae2ea35fc56401ee69106c90f8455
joxon <joxonxaker@gmail.com>
2023-06-30 14:07:02 +0500

    some changes

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users','app','manager',
+    'users', 'app', 'manager',
 ]
 
 MIDDLEWARE = [
